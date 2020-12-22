32189745763efa973432cde379088dd2351cd5ed
Noah Ratcliff <me@nratcliff.com>
2020-12-22 13:09:52 -0500

    fix(dest): start pm2 process with sudio for pigpio access

--- shipitfile.js
+++ shipitfile.js
@@ -96,7 +96,7 @@ module.exports = shipit => {
 	shipit.blTask('pm2-server', async () => {
   await shipit.remote(`pm2 delete -s ${appName} || :`);
   await shipit.remote(
-    `pm2 start ${ecosystemFilePath} --env production --watch true`
+    `sudo pm2 start ${ecosystemFilePath} --env production --watch true`
   );
 });
 }
