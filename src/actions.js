17b3dbe5013ab3075d5e6015db7d43e0a13399ff
Noah Ratcliff <nhrtclff@gmail.com>
2020-12-31 10:34:00 -0500

    fix: gracefully handle wrapping over days

--- src/actions.js
+++ src/actions.js
@@ -4,7 +4,7 @@ import { Interpolation, Easing } from '@tweenjs/tween.js'
 export class Action {
 
 	/** Creates Action object from source properties */
-	constructor (source, lights) {
+	constructor(source, lights) {
 		Object.assign(this, source)
 		this.lights = lights;
 		this.originalValues = []
@@ -31,7 +31,7 @@ export class Action {
  * e.x { id: 0, value: 255 }
  */
 export class SimpleAction extends Action {
-	apply () {
+	apply() {
 		this.values.forEach(l => {
 			var light = this.lights[l.id] // TODO: reference lights by name?
 			if (light) {
@@ -41,7 +41,7 @@ export class SimpleAction extends Action {
 		})
 	}
 
-	undo () {
+	undo() {
 		this.values.forEach(l => {
 			var light = this.lights[l.id]
 			if (light) {
@@ -52,91 +52,117 @@ export class SimpleAction extends Action {
 }
 
 export class TimeAction extends Action {
-	constructor (source, lights) {
-		super (source, lights)
-		this.listeners = {}
+	constructor(source, lights) {
+		super(source, lights)
+		this.animators = {}
 	}
 
-	apply () {
+	apply() {
 		this.timings.forEach(t => {
 			var light = this.lights[t.id]
 			if (light) {
 				this.originalValues[t.id] = light.value
 
-				// create update function
-				this.listeners[t.id] = {}
-				this.listeners[t.id].function = ((light, values) => {
-					console.log(`Updating light ${light.name} from time action`)
-					// current time
-					var current = DateTime.local()
-
-					// get closest time before current time
-					var previous = null
-					var prevIndex = -1
-					for (let i = 0; i < values.length; i++) {
-						var v = values[i];
-						var time = DateTime.fromISO(v.time)
-						if (!previous || time < current) {
-							previous = {
-								time: time,
-								value: v.value
-							}
-							prevIndex = i
-						}
-					}
-
-					// get next time in list
-					var nextIndex = (prevIndex + 1) % values.length
-					var next = values[nextIndex]
-					next = {
-						time: DateTime.fromISO(next.time),
-						value: next.value
-					}
-
-					// calculate current time relative to prev/next
-					var range = next.time.diff(previous.time)
-					var norm = current.diff(previous.time)
-					var progress = norm.milliseconds / range.milliseconds
-					
-					// set light value by progress between previous and current
-					// linear interpolation between [previous.value, next.value]
-					// TODO: support easing functions
-					var t = progress
-					v = Interpolation.Linear([previous.value, next.value], t)
-					v = Math.round(v)
-					console.log(`Value: ${v}`)
-					light.value = v
-				}).bind(null, light, t.values)
-
-				// calculate update frequency from timing values
-				var maxSpeed = null
-				for (let i = 1; i < t.values.length; i++) {
-					var v = t.values[i]
-					var p = t.values[i-1]
-					var td = DateTime.fromISO(v.time).diff(DateTime.fromISO(p.time), "seconds")
-					var vd = v.value - p.value
-					var speed = vd / td.seconds // value change / time
-					if (speed > maxSpeed) maxSpeed = speed
-				}
-
-				console.log(`Update rate for ${light.name} is ${maxSpeed}/s`)
-				this.listeners[t.id].function()
-				this.listeners[t.id].handle = setInterval(
-					this.listeners[t.id].function, 
-					1000 / maxSpeed
-				)
-			} 
+				// create animator
+				this.animators[t.id] = new TimeAnimatedLight(light, t.values)
+				this.animators[t.id].start()
+			}
 		})
 	}
 
-	undo () {
+	undo() {
 		this.timings.forEach(t => {
 			var light = this.lights[t.id]
 			if (light) {
-				clearInterval(this.listeners[t.id].handle)
+				this.animators[t.id].stop()
 				light.value = this.originalValues[t.id]
 			}
 		})
+	}
+}
+
+class TimeAnimatedLight {
+	constructor (light, values) {
+		this.light = light
+		this.values = values
+		this.handle = -1
+	}
+
+	start () {
+		this.update()
+	}
+
+	stop () {
+		clearTimeout(this.handle)
+	}
+
+	update () {
+		console.log(`Updating light ${this.light.name} from time action`)
+		// current time
+		var current = DateTime.local()
+
+		// get closest time before current time
+		var previous = null
+		var prevIndex = -1
+		for (let i = 0; i < this.values.length; i++) {
+			var v = this.values[i];
+			var time = DateTime.fromISO(v.time)
+			if (time <= current) {
+				previous = {
+					time: time,
+					value: v.value
+				}
+				prevIndex = i
+			} else if (i === 0) { // current time is before first value, use last value yesterday
+				v = this.values[this.values.length - 1]
+				previous = {
+					time: DateTime.fromISO(v.time).minus({ days: 1}),
+					value: v.value
+				}
+				prevIndex = -1
+			}
+		}
+
+		// get next time in list
+		var nextIndex = (prevIndex + 1) % this.values.length
+
+		var next = this.values[nextIndex]
+		next = {
+			time: DateTime.fromISO(next.time),
+			value: next.value
+		}
+
+		// add a day to nextif we wrapped into tomorrow time
+		if (nextIndex < prevIndex) {
+			next.time = next.time.plus({ day: 1 })
+		}
+
+		// calculate current time relative to prev/next
+		var range = next.time.diff(previous.time).milliseconds
+		var norm = current.diff(previous.time).milliseconds
+		var progress = norm / range
+
+		// set light value by progress between previous and current
+		// linear interpolation between [previous.value, next.value]
+		// TODO: support easing functions
+		var t = progress
+		v = Interpolation.Linear([previous.value, next.value], t)
+		v = Math.round(v)
+		console.log(`Value: ${v}`)
+		this.light.value = v
+
+		// calculate next update time from current and next timings
+		// rate = |(v2 - v1) / (t2 - t1)|
+		// range = t2 - t1
+		var vd = next.value - previous.value
+		var rate = 0
+		if(vd !== 0) {
+			rate = Math.floor(1 / Math.abs(vd / range))
+		} else { // no value change until next time 
+			rate = range - norm // range - norm = ms to next time
+		}
+		console.log(`Next update in ${rate}`)
+		this.handle = setTimeout(this.update.bind(this), rate)
 	}
 }
 
